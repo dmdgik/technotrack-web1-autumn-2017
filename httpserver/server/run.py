ce72510b5ae846317d3453f4a022cd2d8ac3f8c6
Mikhail Gubarenko <dmdgik@gmail.com>
2017-09-23 23:17:26 +0300

    Home work web 1.1

--- httpserver/server/run.py
+++ httpserver/server/run.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import socket
+import os
 
 
 def get_response(request):
@@ -7,21 +8,41 @@ def get_response(request):
 	directory_start = request.find('GET ')
 	directory_cut = request[directory_start + 4: ]
 	directory_end = directory_cut.find(' ')
-
 	directory = directory_cut[: directory_end]
+
+	directory_media = "/home/mikhail/technotrackWEB/technotrack-web1-autumn-2017/httpserver/files"
+	media_list = os.listdir(directory_media)
+	media_list_str = '\n'.join(media_list)
+
+	if directory[0 : 7] == "/media/" and (directory[7 : ] != "test1.txt" or directory[7 : ] != "test2.txt"): 
+		no_file_flag = 1
+	else: 
+		no_file_flag = 0
 
 	if directory == "/":
 		user_agent_start = request.find('User-Agent: ')
 		user_agent_cut = request[user_agent_start + len('User-Agent: ') :]
 		user_agent_end = user_agent_cut.find('\n')
 		user_agent = user_agent_cut[: user_agent_end]
-		return 'Hello mister!\nYou are: ' + user_agent + '\nYour directory: ' + directory
+		return 'HTTP/1.1 200 OK\n\n' + 'Hello mister!\nYou are: ' + user_agent
+	elif directory == "/test/":
+		return 'HTTP/1.1 200 OK\n\n' + request
+	elif directory == "/media/":
+		return 'HTTP/1.1 200 OK\n\n' + media_list_str
+	elif directory == "/media/" + "test1.txt":
+		file = open(directory_media + "/test1.txt", "r")
+		file_content = file.read()
+		file.close
+		return 'HTTP/1.1 200 OK\n\n' + file_content
+	elif directory == "/media/" + "test2.txt":
+		file = open(directory_media + "/test2.txt", "r")
+		file_content = file.read()
+		file.close
+		return 'HTTP/1.1 200 OK\n\n' + file_content
+	elif no_file_flag == 1:
+		return 'HTTP/1.1 404 Not found\n\n' + 'File not found' 
 	else:
-		return 'Not found' + directory
-
-def send_response(): 
-	pass
-
+		return 'HTTP/1.1 404 Not found\n\n' + 'Page not found' 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto = 0)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -36,7 +57,6 @@ while 1:
         print 'Got new client', client_socket.getsockname()  # при подключении к серверу выводить сообщение
         request_string = client_socket.recv(2048)  # принятия данных от пользователя порциями по 2048 байт
         client_socket.send(get_response(request_string))  # вывод информации из функции get_response
-        client_socket.send('\n\n\n' + request_string)
         client_socket.close()
     except KeyboardInterrupt:  # при нажатии сочетания клавиш Ctrl+C переходит в блок закрытия сокета
         print 'Stopped'
