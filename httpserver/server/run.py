161fc81cc06e3bbceebf239516161cc53d0602e5
Mikhail Gubarenko <dmdgik@gmail.com>
2017-09-24 15:03:13 +0300

    Home work web 1.6

--- httpserver/server/run.py
+++ httpserver/server/run.py
@@ -5,6 +5,9 @@ import os
 
 def get_response(request):
 
+	request_type_end = request.find(' ')
+	request_type = request[: request_type_end]
+
 	directory_start = request.find(' ')
 	directory_cut = request[directory_start + 1: ]
 	directory_end = directory_cut.find(' ')
@@ -14,40 +17,42 @@ def get_response(request):
 	media_list = os.listdir(directory_media)
 	media_list_str = '\n'.join(media_list)
 
-	if directory[0 : 7] != "/media/":
-		no_file_flag = 2
-	else:
-		no_file_flag = 1
-		for i in media_list:
-			if directory[7 :] == i:
-				no_file_flag = 0
-				directory_media_file = i
-				break
+	if request_type == "GET":
 
-	if directory == "/":
-		user_agent_start = request.find('User-Agent: ')
-		user_agent_cut = request[user_agent_start + len('User-Agent: ') :]
-		user_agent_end = user_agent_cut.find('\n')
-		user_agent = user_agent_cut[: user_agent_end]
-		return 'HTTP/1.1 200 OK\n\n' + 'Hello mister!\nYou are: ' + user_agent + "\n"
-	elif directory == "/test/":
-		return 'HTTP/1.1 200 OK\n\n' + request + "\n"
-	elif directory == "/media/":
-		return 'HTTP/1.1 200 OK\n\n' + media_list_str + "\n"
-	elif no_file_flag == 0 and directory == "/media/" + directory_media_file:
-		file = open(directory_media + "/" + directory_media_file, "r")
-		file_content = file.read()
-		file.close
-		return 'HTTP/1.1 200 OK\n\n' + file_content + "\n"
+		if directory[0 : 7] != "/media/":
+			no_file_flag = 2
+		else:
+			no_file_flag = 1
+			for i in media_list:
+				if directory[7 :] == i:
+					no_file_flag = 0
+					directory_media_file = i
+					break
+
+		if directory == "/":
+			user_agent_start = request.find('User-Agent: ')
+			user_agent_cut = request[user_agent_start + len('User-Agent: ') :]
+			user_agent_end = user_agent_cut.find('\n')
+			user_agent = user_agent_cut[: user_agent_end]
+			return 'HTTP/1.1 200 OK\n\n' + 'Hello mister!\nYou are: ' + user_agent + "\n"
+		elif directory == "/test/":
+			return 'HTTP/1.1 200 OK\n\n' + request + "\n"
+		elif directory == "/media/":
+			return 'HTTP/1.1 200 OK\n\n' + media_list_str + "\n"
+		elif no_file_flag == 0 and directory == "/media/" + directory_media_file:
+			file = open(directory_media + "/" + directory_media_file, "r")
+			file_content = file.read()
+			file.close
+			return 'HTTP/1.1 200 OK\n\n' + file_content + "\n"
 	#elif directory == "/media/" + "test2.txt":
 	#	file = open(directory_media + "/test2.txt", "r")
 	#	file_content = file.read()
 	#	file.close
 	#	return 'HTTP/1.1 200 OK\n\n' + file_content + "\n"
-	elif no_file_flag == 1:
-		return 'HTTP/1.1 404 Not found\n\n' + 'File not found' + "\n"
-	else:
-		return 'HTTP/1.1 404 Not found\n\n' + 'Page not found' + "\n"
+		elif no_file_flag == 1:
+			return 'HTTP/1.1 404 Not found\n\n' + 'File not found' + "\n"
+		else:
+			return 'HTTP/1.1 404 Not found\n\n' + 'Page not found' + "\n"
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto = 0)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
